95972b02249274a142e6d22d64f8fbf387cb95dc
aktfrikshun <akt@frikshun.com>
2013-03-28 11:42:11 -0400

    Adding module renci_create_content

---
+++ sites/all/modules/renci_create_content/hydroshare-node-form.tpl.php
@@ -0,0 +1,26 @@
+<?php echo "<h2>Hydroshare Custom Add Group Form</h2>"?>
+
+<div class="node-add-wrapper clear-block">
+
+  <div class="node-column-main">
+    <?php if($form): ?>
+    	<?php
+    	
+    	  // Render all form elements with the exception of the option tabs
+		  $output = '';
+    	  $children_keys = element_children($form);
+		  foreach ($children_keys as $key) {
+		    if (!empty($form[$key])) {
+		      if (!($key == 'additional_settings')) {
+		      	 $output .= drupal_render($form[$key]);
+		      }
+
+		    }
+		  }
+		  print $output;		  
+     ?>
+    <?php endif; ?>
+   
+  </div>
+  <div class="clear"></div>
+</div>

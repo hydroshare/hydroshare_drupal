306e6244efb117fccb351c02a5d94e278f749492
aktfrikshun <akt@frikshun.com>
2013-07-11 12:37:53 -0400

    Test hook form alter

--- sites/all/themes/qcl_hydroshare/template.php
+++ sites/all/themes/qcl_hydroshare/template.php
@@ -8,4 +8,8 @@ if (drupal_is_front_page()) {
 drupal_add_js(drupal_get_path('theme', 'bluemasters') . '/js/bluemasters.js');
 }
 
+function qcl_hydroshare_form_alter(&$form, &$form_state, $form_id) {
+    drupal_set_message("This is the form id : $form_id");
+}
+
 ?>

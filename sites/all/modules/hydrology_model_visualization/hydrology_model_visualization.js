669376a08c39496c8c11f3b31d60b23084dcf61b
Castronova <quickstart@qs10.(none)>
2013-07-29 10:22:41 -0500

    Added dublin core fields to hydrology_model.module

---
+++ sites/all/modules/hydrology_model_visualization/hydrology_model_visualization.js
@@ -0,0 +1,284 @@
+function hydrology_model_plot( model_zip_path ){
+
+  var plotHt = 280;
+
+  // Define margins for all plots. This could be done programatically too if 
+  // the number of charts is not know before hand or is selected by a user.
+  var margin_context = {top:200, right: 10, bottom:20, left: 40};
+      margin2= {top:(120), right: 10, bottom:(20), left: 40};
+
+      width = 700 - margin2.left - margin2.right;
+      height_context = plotHt - margin_context.top - margin_context.bottom;
+      height2  = plotHt - margin2.top - margin2.bottom;
+
+  var parseDate = d3.time.format("%m/%d/%Y %H:%M").parse; // hydroshare default
+
+  //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
+  // Scales.                                              //
+  //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
+  var x_context = d3.time.scale().range([0, width]);
+  var x2 = d3.time.scale().range([0, width]);
+
+  var y_context = d3.scale.linear().range([height_context, 0]);
+  var y2 = d3.scale.linear().range([height2, 0]);
+
+  //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
+  // Axes.                                                //
+  //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
+  var xAxis2			= d3.svg.axis().scale(x_context).orient("bottom"),
+      xAxis 			= d3.svg.axis().scale(x2).orient("bottom"),
+      yAxis 			= d3.svg.axis().scale(y2).orient("left");
+
+  var brush = d3.svg.brush()
+      .x(x_context)
+      .on("brush", brush);
+
+  //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
+  // Area generators.                                     //
+  //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
+  var area = d3.svg.area()
+      .interpolate("linear")
+      .x(function(d) { return x2(d.date); })
+      .y0(height2)
+      .y1(function(d) { return y2(d.value); });
+
+  var area_context = d3.svg.area()
+      .interpolate("linear")
+      .x(function(d) { return x_context(d.date); })
+      .y0(height_context)
+      .y1(function(d) { return y_context(d.value); });
+
+  // Attach rectangles to svg elements that mark boundary of each plot.    
+  var svg = d3.select("#hydrology_model_vizualization").append("svg")
+      .attr("width", width + margin2.left + margin2.right)
+      .attr("height", height2 + margin2.top + margin2.bottom);
+
+  svg.append("defs").append("clipPath")
+      .attr("id", "clip")
+      .append("rect")
+      .attr("width", width)
+      .attr("height", height2);
+
+  // Set position of the different charts created previously.
+  var context = svg.append("g")
+      .attr("transform", "translate(" + margin_context.left + "," + margin_context.top + ")");
+
+  var extra_discharge = svg.append("g")
+      .attr("transform", "translate(" + margin2.left + "," + (margin2.top-90) + ")");
+
+  var data0 = [];
+  var remaining = 3;
+  
+ 
+  
+  // ----------------------------------------------------------
+  var line = d3.svg.line()
+    .x(function(d) { return x_context(d.date); })
+    .y(function(d) { return y_context(d.close); });
+    
+  var arr_data = [
+    ["1/1/2013 00:00", 200],
+    ["2/1/2013 00:00", 500],
+    ["3/1/2013 00:00", 300],
+    ["4/1/2013 00:00", 100],
+    ["5/1/2013 00:00", 400],
+    ["6/1/2013 00:00", 300]];
+    
+    var data = arr_data.map(function(d){
+      return {
+        date: parseDate(d[0]),
+        close: d[1]
+      };
+    });
+        
+  x_context.domain(d3.extent(data, function(d) { return d.date; }));
+  y_context.domain(d3.extent(data, function(d) { return d.close; }));
+  
+  svg.append("path")
+      .datum(data)
+      .attr("class", "line")
+      .attr("d", line);
+  
+  
+  // ----------------------------------------------------------
+  
+  
+  // unzip the model 
+  zip.createReader(new zip.HttpReader(model_zip_path), function(reader) {
+
+    // get entries from zip
+    reader.getEntries(function(entries) {
+      if (entries.length){
+        entries[0].getData(new zip.TextWriter(), function (text) {
+          var t = text;
+          
+          // close the zip reader
+          reader.close(function(){
+          });
+      }, function (current, total){
+         });
+      }
+    });
+  },
+  function(error){
+    console.log(error);
+  });
+
+  
+//  csvfilename  =  model_zip_path.replace(/\/[^\/]*$/, '/test_data_series.csv')
+//  path  =  model_zip_path.replace(/\/[^\/]*$/, '/test_data_series.csv')
+
+  
+//  function csv(path, callback) {
+//      d3.csv(path, function(csv) {
+//      csv ? callback(null, csv) : callback("error", null);
+//      });
+//  };
+
+
+  // Work around for D3's data file reading in asynchronous manner. Nested read commands
+  // for different csv files and plotting functions are called in the inner-most loop, once 
+  // all data files have been read.  A more elegant solution is needed in case 
+  // number of input files is not previously know. 
+
+//  d3.csv( csvfilename, function(src1) {
+
+          // Do something with src1, src2, and src3.
+//          data0.push(src1);
+
+    // Extract date and value from each file.				
+//    data0[0].forEach( function(d,i){
+  //		d.date = parseDate(d.date);
+  //		d.value = (+d.value);
+//                  d.date = parseDate(d.DateTimeUTC);   // hydroshare format
+//                  d.value = (+d.DataValue);            // hydroshare format
+//    });
+//    data0[0].mean = 0;
+
+    //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
+    // Define domains of scales.                            //
+    //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
+
+    x2.domain(d3.extent(data0[0].map(function(d) { return d.date; })));
+    y2.domain([0, d3.max(data0[0].map(function(d) { return (d.value); }))]);
+    console.log( "max discharge: " + d3.max(data0[0].map(function(d) { return (d.value); })) );
+
+    x_context.domain(x2.domain());
+    y_context.domain(y2.domain());
+
+          Render( file_metadata_title, file_metadata_source );
+    });
+
+  function Render( the_title, the_source )
+  {
+
+    //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
+    // Graph for main area.                                 //
+    //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
+
+    extra_discharge.append("rect")
+      .attr("fill","#0088ff")
+      .attr("width",width)
+      .attr("height",height2)
+      .attr("opacity", 0.08);
+
+    extra_discharge.append("path")
+      .datum(data0[0])
+      .attr("clip-path", "url(#clip)")
+      .attr("d", area)
+      .attr("fill","#0070dd")
+      .attr("opacity", .35);
+
+    extra_discharge.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height2 + ")")
+      .call(xAxis);
+
+    extra_discharge.append("g")
+      .attr("class", "y axis")
+      .call(yAxis);
+
+    extra_discharge.append("text")
+        .text(the_title)
+        .attr("transform","translate("+(10) + "," + (-16) + ")");
+
+    extra_discharge.append("text")
+        .text(the_source)
+        .attr("transform","translate(" + (10) + "," + (-4) + ")");
+
+    //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
+    // Graph for context area.                              //
+    //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
+
+    context.append("text")
+        .text("Select area to Zoom -- Drag selection to Pan -- Click away to Reset")
+        .attr("transform","translate("+(10) + "," + (10) + ")");
+
+  //	context.append("text")
+  //			.text("Click away to unselect")
+  //			.attr("transform","translate(" + (10) + "," + (20) + ")");
+
+    context.append("path")
+      .datum(data0[0])
+      .attr("d", area_context)
+      .attr("fill","#0070dd")
+      .attr("opacity",.2);
+
+    context.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height_context + ")")
+          .call(xAxis2);
+
+    context.append("g")
+      .attr("class", "x brush")
+      .call(brush)
+      .selectAll("rect")
+      .attr("y", -6)
+      .attr("height", height_context + 7);
+
+  }
+
+
+  function brush() {
+    x2.domain(brush.empty() ? x_context.domain() : brush.extent());
+
+    //console.log("selected range:" + brush.extent()[0] + "  , " + brush.extent()[1] + " >> " + brush.extent());
+
+    extra_discharge.select("path").attr("d", area);
+    extra_discharge.select(".x.axis").call(xAxis);
+
+    data0[0].mean = brush.extent() > 0 ? 0 : d3.mean( data0[0].map( function(d,i) {
+        if(d.date >= brush.extent()[0] && d.date <= brush.extent()[1])
+          return d.value;
+      })
+    );
+
+   // Compute averages for selection window.
+
+    svg.selectAll("#avg_line")
+      .remove();
+    svg.selectAll("#avg_text")
+      .remove();
+
+    // Discharge. 
+    if(!isNaN(y2(data0[0].mean)) )
+    {
+
+      extra_discharge.append("svg:line")
+        .attr("id","avg_line")
+      .attr("x1",0)
+      .attr("y1", function(d,i) { return y2(data0[0].mean); } )
+      .attr("x2",width)
+      .attr("y2", function(d,i) { return y2(data0[0].mean); } )
+      .attr("stroke","#000088");
+
+    extra_discharge.append("svg:text")
+      .text("Avg:"+(d3.format(',.2f')(data0[0].mean)))
+      .attr("id","avg_text")	
+      .attr("transform","translate("+(width-60)+","+(y2(data0[0].mean)-5)+")");
+    }
+
+  }
+
+}
+
---
+++ sites/all/themes/qcl_hydroshare/node--hydroshare-hydrology-model.tpl.php
@@ -0,0 +1,165 @@
+<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
+
+    <?php
+    if( !empty( $content ) ) {
+        
+      // add javascript and css  
+      drupal_add_js('http://d3js.org/d3.v3.js', 'external');
+      drupal_add_css(drupal_get_path('module','hydrology_model_visualization').'/hydrology_model_visualization.css', 'file');
+      drupal_add_js(drupal_get_path('module','hydrology_model_visualization').'/hydrology_model_visualization.js', 'file');
+      drupal_add_js(drupal_get_path('module','hydrology_model_visualization')."/zip.js", 'file');
+      
+        // get the file directory
+        $real_path = ( $node->field_file['und'][0]['uri'] ); 
+        $dir = drupal_dirname( $real_path );
+
+        if( $teaser ) {
+          
+            print render($title_prefix); 
+        
+            // =-=-=-=-=-=-=-
+            // display the viz thumbnail
+            $img_url = file_create_url( $dir.'/thumbnail.png' );
+            $img_tag = '<a href="'.$node_url.'"><img src='.$img_url.' width=160 height=120 class=floatLeft ></a>';
+            print( $img_tag );
+        
+            // =-=-=-=-=-=-=-
+            // print the description and truncate the 
+            // content to 1024 characters     
+            $body = render( $content['body'] );
+            print( substr( $body, 0, 1024 ) );
+
+        } else {
+            print $user_picture; 
+            print render($title_prefix); 
+            print( '<div class="subHeader">' );
+            print( '<h1'. $title_attributes.'><a href="'.$node_url.'">'.$title.'</a></h1>');
+            print('<p>Resource Details</p>
+                   </div><!-- subHeader -->
+            <BR><BR><BR>
+            <div class="myContentSub">
+                <div class="myContentSubInner">');
+
+     
+                        // ********************************
+                        // * Get Model Metadata from Node *
+                        // ********************************
+                        $begin = $node->field_temporal_begin['und'][0]['safe_value']; 
+                        $end = $node->field_temporal_end['und'][0]['safe_value']; 
+                        $interval = $node->field_temporal_interval['und'][0]['safe_value']; 
+                        $desc = $node->field_id_description['und'][0]['safe_value']; 
+                        $version = $node->field_id_version['und'][0]['safe_value'];  
+                        $create_dt = $node->field_dev_create_date['und'][0]['safe_value']; 
+                        $contrib_dt = $node->field_dev_contrib_date['und'][0]['safe_value']; 
+                        //$creator = $node->field_dev_creator['und'][0]['safe_value']; 
+                        //$contrib = $node->field_dev_contributor['und'][0]['safe_value']; 
+                        
+                        // these text lists dont have 'safe_value'
+                        $model_type = $node->field_id_type['und'][0]['value']; 
+                        $level   = $node->field_dev_level['und'][0]['value']; 
+                        
+
+                        // **************************
+                        // * get the file directory *
+                        // **************************
+
+                        // get the zip name of the directory
+                        $zip_file = file_create_url($real_path);
+                        $zip_file = str_replace("http://hydroshare", ".", $zip_file);
+                        $zip_file = str_replace('.local', '', $zip_file);
+                        
+                        // build the java script operation
+                        $hostname = $_SERVER['SERVER_NAME'];
+                        $op = "http://$hostname/export.php?file=" . $zip_file;
+     
+                        $edit_url = "http://$hostname/?q=node/" . $node->nid . "/edit";
+                        $delete_url = "http://$hostname/?q=node/" . $node->nid . "/delete";
+                        print( '<div class="contentListWrapper">');
+                            //print( '<a href="" class="greyButton">EXECUTE</a>'); 
+                            print( '<a href="" class="greyButton">SHARE</a>');
+                            print( '<a href="'.$op.'" class="greyButton">EXPORT</a>');
+                            print( '<a href="'.$edit_url.'" class="greyButton">EDIT</a>');
+                            print( '<a href="'.$delete_url.'" class="greyButton">DELETE</a>');
+                            // TODO: Add data visualization button
+                        print( '</div> <!-- contentListWrapper --> ');
+
+
+                        // plot model data
+                        print( '<div id="hydrology_model_vizualization" style="height:340px"></div>' );
+                        // create a file instance from uploaded model
+                        $fid =  $node->field_file['und'][0]['fid'];
+                        $f = file_load($fid);   
+                        $url = file_create_url($f->uri);
+
+                        // get the absolute path of the upload file
+                        $wrapper = file_stream_wrapper_get_instance_by_uri($f->uri);
+                        $real_path = $wrapper->realpath();
+                        //$path_array = explode('/',$real_path);
+                        print( "<script>hydrology_model_plot('".$zip_file."');</script>");
+          
+                        
+                        $type    = node_type_get_name( $node ); 
+                        $user = user_load(array('uid' => $node->uid));
+
+                        print('<div style="clear:left"><br /><br /><br /></div>');
+
+                        print( '<div class="half-column">' );
+                            print( '<p><span class="bold">Resource Type:</span> '.$type.'</p>');
+                            print( '<p><span class="bold">Model Type: </span>'.$model_type.'</p>');
+                            print( '<p><span class="bold">Created by:</span> '.$user->name.'</p>');
+                            print( '<p><span class="bold">Created: </span>'.$contrib_dt.'</p>');
+                            // ratings
+                            print( '<div class="starWrapper">');
+                                print( render( $content['field_rating'] ) );
+                            print('</div>');
+                            // tags
+                            $tags = $node->field_tags['und'];
+                            print( '<p><span class="bold">Tags: </span>' );
+                            foreach( $tags as $tag ) {
+                                print( $tag['taxonomy_term']->name.', ');
+                            }
+                            print( '</p>' );
+                        print( '</div> <!-- half-column -->' );
+          
+                        print( '<div class="half-column-right">' );
+
+                         
+                            print( '<p><span class="bold">Contributors: </span>'.$contrib.'</p>');
+                            print( '<p><span class="bold">Model Begin: </span>'.$begin.'</p>');
+                            print( '<p><span class="bold">Model End: </span>'.$end.'</p>');
+                            print( '<p><span class="bold">Time Interval </span>'.$interval.'</p>');
+                            print( '<p><span class="bold">Development Level: </span>'.$level.'</p>');
+                            print( '<p><span class="bold">Version: </span>'.$version .'</p>');
+                            print( '<p><span class="bold">Date Created: </span>'.$create_dt.'</p>');
+                            print( '<p><span class="bold">Format: </span>'.'zip'.'</p>');
+                        print( '</div> <!-- half-column-right -->' );
+          
+                        print('<div style="clear:both"></div>');
+
+                        print('<div class="full-column">');
+                            print('<h2>Resource Description</h2>');
+                            //print( render( $content['body'] ) );
+                            print( $desc );
+                        print( '</div> <!-- full-column -->' );
+                  
+                        print( '<div class="half-column">');
+                            if (!empty($content['links'])) {
+                                print( ' <div class="links">' );
+                                    print render($content['links']);            
+                                print( '</div>');
+                            }
+                        print( '</div>' );
+
+                    print( '</div> <!-- myContentSubInner -->');
+             
+              print('</div> <!-- mycontentSub -->');
+
+              print('<div style="clear:both"></div>');
+              print( render($content['comments']) ); 
+
+        }
+
+    } 
+?>
+   
+</div>

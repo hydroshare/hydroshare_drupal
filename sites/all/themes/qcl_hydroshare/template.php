ce5e6a0c0f1b5a9a8e011bc501b123c6211c5e5f
David Valentine <david.valentine@gmail.com>
2013-09-26 20:30:20 -0700

    fix wrong theme reference. This is the qcl_hydroshare not bluemasters

--- sites/all/themes/qcl_hydroshare/template.php
+++ sites/all/themes/qcl_hydroshare/template.php
@@ -5,7 +5,7 @@
  * Add javascript files for front-page jquery slideshow.
  */
 if (drupal_is_front_page()) {
-drupal_add_js(drupal_get_path('theme', 'bluemasters') . '/js/bluemasters.js');
+drupal_add_js(drupal_get_path('theme', 'qcl_hydroshare') . '/js/bluemasters.js');
 }
 
 
